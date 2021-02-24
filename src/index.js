20b44740230ab91b6eb6076eced521a6daa60c5c
Javier Ignacio De la Rosa Vitar <javier.delarosa@pacificored.cl>
2021-02-24 17:49:37 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//const element = document.createElement("h1");
+//element.innerText = "Hello React";
+//const container = document.getElementById("root");
+//container.appendChild(element);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./components/Card/index";
+import "bootstrap/dist/css/bootstrap.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+
+// ReactDOM.render(__QUE__,__DONDE__)
+ReactDOM.render(<Card></Card>, container);
